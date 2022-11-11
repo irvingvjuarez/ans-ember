7338bb0b0be982a74620f38745d7827bad8065df
Irving Juárez <irvingjuarez274@gmail.com>
2022-11-11 15:30:56 -0600

    Users splitted corrected in the first challenge

--- challenge1/index.js
+++ challenge1/index.js
@@ -1,5 +1,7 @@
 import { users } from "./users.js"
+const splitter = new RegExp("(\\n|\n)", "g")
 
-const usersArr = users.split("\n\n")
+const required = ["usr", "eme", "psw", "age", "loc", "fll"]
+const usersArr = users.split("\n\n").map(user => user.replace(splitter, " "))
 
-console.log(usersArr.length)
+console.log(usersArr)
