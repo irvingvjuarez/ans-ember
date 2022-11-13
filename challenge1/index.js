943b9d568d21e0a1f78c8e2cb72811130d83128e
Irving Juárez <irvingjuarez274@gmail.com>
2022-11-12 18:19:41 -0600

    First challenge solved correctly

--- challenge1/index.js
+++ challenge1/index.js
@@ -1,7 +1,15 @@
 import { users } from "./users.js"
 const splitter = new RegExp("(\\n|\n)", "g")
 
-const required = ["usr", "eme", "psw", "age", "loc", "fll"]
+const requiredProps = ["usr", "eme", "psw", "age", "loc", "fll"]
 const usersArr = users.split("\n\n").map(user => user.replace(splitter, " "))
+const usersProps = usersArr.map(user => {
+  user = user.split(" ")
+  const userObj = {}
+  user.forEach(prop => {userObj[prop.substring(0,3)] = prop.substring(3)})
+  return userObj
+})
 
-console.log(usersArr)
+const validUsers = usersProps.filter(props => requiredProps.every(prop => prop in props))
+
+console.log(validUsers.length)
