98657140e3a1968780696202fe506f9b8b5b8315
Irving Juárez <irvingjuarez274@gmail.com>
2022-11-15 16:24:36 -0600

    Message decoded

--- challenge2/index.js
+++ challenge2/index.js
@@ -4,6 +4,30 @@ import path from "path"
 const encryptedTextPath = path.resolve("encrypted.txt")
 const messageBuffer = fs.readFileSync(encryptedTextPath)
 const messageTextContent = messageBuffer.toString()
+
+const decodedMessage = messageTextContent.split(" ")
+	.map(word => {
+		word = word.split("")
+		const asciiWord = []
+		let asciiCode
+
+		while(word.length > 0) {
+			asciiCode = word.splice(0, 2).join("")
+			const isValid = asciiCode >= 97 && asciiCode <= 122
+
+			if (!isValid) {
+				const missingNumber = word.splice(0, 1).join("")
+				asciiCode += missingNumber
+			}
+
+			asciiWord.push(asciiCode)
+		}
+
+		const decodedWord = asciiWord.map(letterCode => String.fromCharCode(letterCode)).join("")
+
+		return decodedWord
+	})
+
 console.log({
-    messageTextContent
+	decodedMessage
 })
