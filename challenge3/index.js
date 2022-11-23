054277e7a43e014a020d023a7fd54315767ca3e9
Irving Juárez <irvingjuarez274@gmail.com>
2022-11-23 11:28:12 -0600

    Able to identify where a sequence starts and finishes

--- challenge3/index.js
+++ challenge3/index.js
@@ -1,4 +1,36 @@
 import fs from "fs"
 
+let firstColor = "", secondColor = "", sequenceCount = 0
 const colors = JSON.parse(fs.readFileSync("./colors.json"))
-console.log(colors)
+
+function printValues() {
+	console.log({
+		firstColor,
+		secondColor,
+		sequenceCount
+	})
+}
+
+for(let color of colors) {
+	if (sequenceCount === 0) {
+		firstColor = color;
+		sequenceCount ++
+	} else if (color !== firstColor && sequenceCount === 1) {
+		secondColor = color
+		sequenceCount ++
+	} else if (sequenceCount > 1) {
+		const isInFirstColor = sequenceCount % 2 === 0;
+
+		if (isInFirstColor && color !== firstColor) {
+			printValues()
+			sequenceCount = 0
+			break;
+		} else if (!isInFirstColor && color !== secondColor) {
+			printValues()
+			sequenceCount = 0
+			break;
+		} else {
+			sequenceCount ++
+		}
+	}
+}
